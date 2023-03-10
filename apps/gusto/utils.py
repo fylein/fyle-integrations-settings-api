4ad03a34516223aaa462feea3ab3f3fe222c931c
Vishal Pandey <vishalpandeyviptsk@gmail.com>
2023-03-10 14:42:30 +0530

    updated migrations and gusto menifest file

--- apps/gusto/utils.py
+++ apps/gusto/utils.py
@@ -3,10 +3,9 @@ from workato import Workato
 
 def set_gusto_properties(managed_user_id):
 
-    # Function for Creating a Managed user in Workato
     connector = Workato()
     # Payload for setting up Global Properties in workato to be used
-    # By the fyle workato sdk
+    # By the gusto workato sdk
     properties_payload = {
         'properties': {
             'GUSTO_CLIENT_ID': settings.GUSTO_CLIENT_ID,
@@ -15,5 +14,5 @@ def set_gusto_properties(managed_user_id):
         }
     }
 
-    # Setting Up Properties in Workato, to be used by fyle sdk
+    # Setting Up Properties in Workato, to be used by Gusto sdk
     connector.properties.post(managed_user_id, properties_payload)
