87e7f7c480fc38c0d7922174d18f7b8e63424263
Vishal Pandey <vishalpandeyviptsk@gmail.com>
2023-03-10 19:23:05 +0530

    updated url and gusto menifest

--- apps/gusto/urls.py
+++ apps/gusto/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import PostFolder, PostPackage, GustoConfigurationView, SyncEmployeesView, GustoView
 
 urlpatterns = [
-    path('home/', GustoView.as_view(), name="gusto-home"),
+    path('/', GustoView.as_view(), name="gusto"),
     path('packages/', PostPackage.as_view(), name='package'),
     path('folder/', PostFolder.as_view(), name='folder'),
     path('configuration/', GustoConfigurationView.as_view(), name='configuration'),
