88ec9dd2468c1cad3732078a964bd426753d96cf
Nilesh Pant <nileshpant6969@gmail.com>
2023-03-16 13:51:03 +0530

    Travelperk Tests Fixed

--- tests/test_travelperk/fixtures.py
+++ tests/test_travelperk/fixtures.py
@@ -4,6 +4,7 @@ fixture = {
        "folder_id":"1231",
        "package_id":"1231",
        "is_fyle_connected":"t",
+       "travelperk_connection_id": None,
        "is_s3_connected":"dtmmy",
        "created_at":"2022-11-29T15:39:49.221955Z",
        "updated_at":"2022-11-29T15:41:59.535831Z",
