74170d10cb60490a697e167a32029b83a258bd31
Dhaarani <55541808+DhaaraniCIT@users.noreply.github.com>
2023-03-16 13:32:57 +0530

    gusto auth workato connection (#36)
    gusto auth workato connection

--- admin_settings/settings.py
+++ admin_settings/settings.py
@@ -218,7 +218,8 @@ FYLE_APP_URL = os.environ.get('APP_URL')
 SENDGRID_API_KEY = os.environ.get('SENDGRID_API_KEY')
 SENDGRID_EMAIL = os.environ.get('SENDGRID_EMAIL')
 BASE_URI = os.environ.get('BASE_URI')
-
+WK_JWT_PRIVATE_KEY = os.environ.get('WK_JWT_PRIVATE_KEY')
+WK_API_KEY = os.environ.get('WK_API_KEY')
 
 CORS_ORIGIN_ALLOW_ALL = True
 
@@ -230,8 +231,6 @@ CORS_ALLOW_HEADERS = [
 
 # Environ Variable Required for Tests
 FYLE_REFRESH_TOKEN = os.environ.get('FYLE_REFRESH_TOKEN')
-
-# Environment variables required to integrate Gusto
 GUSTO_CLIENT_ID = os.environ.get('GUSTO_CLIENT_ID')
 GUSTO_CLIENT_SECRET = os.environ.get('GUSTO_CLIENT_SECRET')
 GUSTO_ENVIRONMENT = os.environ.get('GUSTO_ENVIRONMENT')
--- apps/gusto/urls.py
+++ apps/gusto/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import PostFolder, PostPackage, GustoConfigurationView, SyncEmployeesView, GustoView
+from .views import PostFolder, PostPackage, GustoConfigurationView, SyncEmployeesView, GustoView, GustoConnection
 
 urlpatterns = [
-    path('/', GustoView.as_view(), name="gusto"),
+    path('', GustoView.as_view(), name="gusto"),
     path('packages/', PostPackage.as_view(), name='package'),
     path('folder/', PostFolder.as_view(), name='folder'),
     path('configuration/', GustoConfigurationView.as_view(), name='configuration'),
     path('refresh_employees/', SyncEmployeesView.as_view(), name='sync-employees'),
+    path('gusto_connection/', GustoConnection.as_view(), name='fyle-gusto-connection'),
 ]
