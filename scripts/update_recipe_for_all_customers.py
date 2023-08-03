ba0e841f8645f06110e1dbada13de8d292ac7cf8
ruuushhh <rushikeshtayade2001@gmail.com>
2023-08-03 16:03:12 +0530

    Assign expense travelperk

--- scripts/update_recipe_for_all_customers.py
+++ scripts/update_recipe_for_all_customers.py
@@ -16,33 +16,32 @@ def post_package_to_workato():
     count=0
     for org_id in org_ids:
         org = Org.objects.get(id=org_id)
-        if org.managed_user_id:
-            managed_user = connector.managed_users.get_by_id(org.fyle_org_id)
-            if managed_user:
-                travelperk = TravelPerk.objects.filter(org_id = org_id).first()
-                if travelperk:
-                    travelperk_conf = TravelPerkConfiguration.objects.filter(org_id=org_id).first()
-                    if travelperk_conf.is_recipe_enabled: 
-                        try:
-                            connector.recipes.post(org.managed_user_id, travelperk_conf.recipe_id, None, 'stop')
-                        except:
-                            print("recipe not found")
+        managed_user = connector.managed_users.get_by_id(org.fyle_org_id)
+        if org.managed_user_id and managed_user:
+            travelperk = TravelPerk.objects.filter(org_id = org_id).first()
+            if travelperk:
+                travelperk_conf = TravelPerkConfiguration.objects.filter(org_id=org_id).first()
+                if travelperk_conf.is_recipe_enabled: 
                     try:
-                        package = post_package(
-                        org_id=org_id,
-                        folder_id=travelperk.folder_id,
-                        package_path='assets/travelperk.zip'
-                        )
+                        connector.recipes.post(org.managed_user_id, travelperk_conf.recipe_id, None, 'stop')
                     except:
-                        print("package not posted")
-                    travelperk.package_id = package['id']
-                    travelperk.save()
-                    if travelperk_conf.is_recipe_enabled: 
-                        try:          
-                            connector.recipes.post(org.managed_user_id, travelperk_conf.recipe_id, None, 'start')
-                        except:
-                            print("recipe not found")
-                    count +=1
-                    print(org.name, count)
-                    return "Success"
+                        print("recipe not found")
+                try:
+                    package = post_package(
+                    org_id=org_id,
+                    folder_id=travelperk.folder_id,
+                    package_path='assets/travelperk.zip'
+                    )
+                except:
+                    print("package not posted")
+                travelperk.package_id = package['id']
+                travelperk.save()
+                if travelperk_conf.is_recipe_enabled: 
+                    try:          
+                        connector.recipes.post(org.managed_user_id, travelperk_conf.recipe_id, None, 'start')
+                    except:
+                        print("recipe not found")
+                count +=1
+                print(org.name, count)
+                return "Success"
     return "No Orgs Found"
