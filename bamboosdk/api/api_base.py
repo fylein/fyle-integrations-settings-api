0e63fc6d402be305f5ce17d68a64222138328ac8
Ashutosh619-sudo <ashutoshsinghrkt@gmail.com>
2023-12-11 16:03:14 +0530

    minor change

--- bamboosdk/api/api_base.py
+++ bamboosdk/api/api_base.py
@@ -13,7 +13,7 @@ class ApiBase:
         self.__api_token = None
         self.__sub_domain = None
     
-    def post_request(self, module_api_path):
+    def __post_request(self, module_api_path):
         """
         HTTP post method to send data to BambooHR API URL
 
