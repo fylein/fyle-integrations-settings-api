f7ad5a426236557a35fad888415356cbb0cf987e
Ashutosh619-sudo <ashutoshsinghrkt@gmail.com>
2023-12-11 16:22:14 +0530

    class to sync employees from bamboo hr

---
+++ bamboosdk/api/employee.py
@@ -0,0 +1,13 @@
+from .api_base import ApiBase
+
+
+class Employee(ApiBase):
+    
+    GET_EMPLOYEE_REPORT = '/v1/reports/custom?format=JSON&onlyCurrent=false'
+
+    def sync_employees(self):
+        """Get the list of employees from bambooHr
+        Returns:
+            List with dicts in Employee schema.
+        """
+        return self.__post_request(self.GET_EMPLOYEE_REPORT)
