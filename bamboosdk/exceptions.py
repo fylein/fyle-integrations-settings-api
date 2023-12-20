6fd47e959308f1d40a3328d84c7677d9bd076d89
Ashutosh singh <55102089+Ashutosh619-sudo@users.noreply.github.com>
2023-12-20 17:35:33 +0530

    Bamboo sdk base api class  (#97)
    * bambbo sdk base api class
    
    * minor change
    
    * minor changes
    
    * comment resolve
    
    * resolved comments
    
    * added new methods
    
    * added 201 status code
    
    * class to sync employees from bamboo hr (#98)
    
    * class to sync employees from bamboo hr
    
    * bug fix
    
    * minor changes
    
    * comment resolved
    
    * minor changes
    
    * Bamboo sdk class (#99)
    
    * bamboo sdk class
    
    * comment resolved
    
    * Webhook create and delete API  (#108)
    
    * webhook api added
    
    * indentation fixed
    
    * indentaion fixed
    
    * minor fix
    
    * Fields get API (#109)
    
    * Fields get API
    
    * added timeoff instead of fields and tested

---
+++ bamboosdk/exceptions.py
@@ -0,0 +1,30 @@
+"""
+BambooHR SDK exceptions
+"""
+
+
+class BambooHrSDKError(Exception):
+    """
+    The base exception class for BambooHR SDK
+    """
+
+    def __init__(self, msg, response=None):
+        super(BambooHrSDKError, self).__init__(msg)
+        self.message = msg
+        self.response = response
+
+    def __str__(self):
+        return repr(self.message)
+
+
+class NoPrivilegeError(BambooHrSDKError):
+    """The user has insufficient privilege, 403 error."""
+
+class NotFoundItemError(BambooHrSDKError):
+    """Not found the item from URL, 404 error."""
+
+class InvalidTokenError(BambooHrSDKError):
+    """Invalid or non-existing access token, 401 error"""
+
+class InternalServerError(BambooHrSDKError):
+    """Internal server error, 500 error"""
