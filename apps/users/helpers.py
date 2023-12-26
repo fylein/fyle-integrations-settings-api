d9aa46fc22fe35741a36b1d87af1076587040826
Ashutosh singh <55102089+Ashutosh619-sudo@users.noreply.github.com>
2023-12-26 21:51:30 +0530

    Implementaion of sync method for fyle and bamboohr (#112)
    * Implementaion of sync method for fyle and bamboohr
    
    * sync employees method added to platform connector
    
    * implementation of sync fyle and bamboo hr
    
    * removing api_token
    
    * indent correction
    
    * comment resolved
    
    * minor bug fix
    
    * better code
    
    * Import sync department (#115)
    
    * Syncing Department from Bamboohr to Fyle
    
    * resolved commented code
    
    * better optimized code
    
    * comment resolved
    
    * Employee and Approver Import (#116)
    
    * Employee and Approver Import
    
    * bug fix and comment resolved

--- apps/users/helpers.py
+++ apps/users/helpers.py
@@ -1,8 +1,9 @@
 from django.conf import settings
 import json
-from typing import Dict
+from typing import Dict, List
 import requests
 from fyle.platform import Platform
+from apps.fyle_hrms_mappings.models import ExpenseAttribute
 
 
 class PlatformConnector:
@@ -20,8 +21,54 @@ class PlatformConnector:
             client_secret=settings.FYLE_CLIENT_SECRET,
             refresh_token=refresh_token
         )
+    
+    def bulk_post_employees(self, employees_payload):
+        self.connection.v1beta.admin.employees.invite_bulk({'data': employees_payload})
 
+    def get_department_generator(self, query_params):
+        departments = self.connection.v1beta.admin.departments.list_all(query_params={
+            'order': 'id.desc'
+        })
+        return departments
 
+    def post_department(self, department):
+        self.connection.v1beta.admin.departments.post({"data": department})
+    
+    def bulk_create_or_update_expense_attributes(self, attributes: List[dict], attribute_type, org_id, update_existing: bool = False) -> None:
+        """
+        Bulk creates or updates expense attributes.
+        :param attributes: List of expense attributes.
+        :param update_existing: If True, updates/creates the existing expense attributes.
+        """
+        ExpenseAttribute.bulk_create_or_update_expense_attributes(
+            attributes, attribute_type, org_id, update_existing
+        )
+
+    def sync_employees(self, org_id):
+        query_params = {'is_enabled': 'eq.true','order': 'updated_at.desc'}
+        attribute_type = 'EMPLOYEE'
+        generator = self.connection.v1beta.admin.employees.list_all(query_params)
+        for items in generator:
+            employee_attributes = []
+            for employee in items['data']:
+                employee_attributes.append({
+                        'attribute_type': attribute_type,
+                        'display_name': attribute_type.replace('_', ' ').title(),
+                        'value': employee['user']['email'],
+                        'source_id': employee['id'],
+                        'active': True,
+                        'detail': {
+                            'user_id': employee['user_id'],
+                            'employee_code': employee['code'],
+                            'full_name': employee['user']['full_name'],
+                            'location': employee['location'],
+                            'department': employee['department']['name'] if employee['department'] else None,
+                            'department_id': employee['department_id'],
+                            'department_code': employee['department']['code'] if employee['department'] else None
+                        }
+                    })
+        
+        self.bulk_create_or_update_expense_attributes(employee_attributes, attribute_type, org_id, True)
 
 def post_request(url: str, body: Dict, api_headers: Dict) -> Dict:
     """
