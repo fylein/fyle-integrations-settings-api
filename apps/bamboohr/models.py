14e9beea0cf91f163021da940f52bda1f2547cc1
Ashutosh singh <55102089+Ashutosh619-sudo@users.noreply.github.com>
2023-12-27 14:40:46 +0530

    New employee exported at field added to Bamboohr table (#117)
    * New employee exported at field added to Bamboohr table
    
    * employees exported update at exporting employees
    
    * comment resolved

--- apps/bamboohr/models.py
+++ apps/bamboohr/models.py
@@ -17,6 +17,7 @@ class BambooHr(models.Model):
     sub_domain = models.CharField(max_length=255, null=True, help_text='Bamboo HR Sub Domain')
     created_at = models.DateTimeField(auto_now_add=True, help_text='Created at datetime')
     updated_at =  models.DateTimeField(auto_now=True, help_text='Updated at datetime')
+    employee_exported_at = models.DateTimeField(auto_now_add=True, help_text='Employee exported to Fyle at datetime')
 
     class Meta:
         db_table = 'bamboohr'
