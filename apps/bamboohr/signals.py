11f8bff46096385ec20edecbf3f910f1d71fae82
Ashutosh619-sudo <ashutoshsinghrkt@gmail.com>
2024-01-02 21:50:58 +0530

    create and delete webhook added

--- apps/bamboohr/signals.py
+++ apps/bamboohr/signals.py
@@ -1,9 +1,12 @@
 
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, post_save, pre_delete
 from django.dispatch import receiver
+from admin_settings.settings import API_URL
 
 from workato import Workato
-from apps.bamboohr.models import BambooHrConfiguration
+
+from bamboosdk.bamboohrsdk import BambooHrSDK
+from apps.bamboohr.models import BambooHrConfiguration, BambooHr
 from apps.orgs.models import Org
 
 @receiver(pre_save, sender=BambooHrConfiguration)
@@ -11,3 +14,34 @@ def run_pre_save_configuration_triggers(sender, instance: BambooHrConfiguration,
     connector = Workato()
     org = Org.objects.get(id=instance.org_id)
     connector.recipes.post(org.managed_user_id, instance.recipe_id, None, 'stop')
+
+
+@receiver(post_save, sender=BambooHr)
+def run_post_save_bamboohr_triggers(sender, instance: BambooHr, **kwargs):
+    bamboohrsdk = BambooHrSDK(api_token=instance.api_token, sub_domain=instance.sub_domain)
+    
+    webhook_payload = {
+        "postFields": {
+            "firstName": "firstName",
+            "lastName": "lastName",
+            "department": "department",
+            "workEmail": "workEmail",
+            "supervisorEmail": "supervisorEmail",
+            "status": "status"
+        },
+        "name": instance.org.name,
+        "monitorFields": ["firstName", "lastName", "department", "workEmail", "supervisorEmail", "status"],
+        "url": API_URL+f'/orgs/{instance.org.id}/bamboohr/webhook_callback/',
+        "format": "json"
+    }
+
+    response = bamboohrsdk.webhook.post(payload=webhook_payload)
+    instance.webhook_id = int(response['id'])
+    instance.save()
+
+@receiver(pre_delete, sender=BambooHr)
+def run_pre_delete_bamboohr_trigger(sender, instance: BambooHr, **kwargs):
+    webhook_id = instance.webhook_id
+
+    bambamboohrsdk = BambooHrSDK(api_token=instance.api_token, sub_domain=instance.sub_domain)
+    bambamboohrsdk.webhook.delete(id=webhook_id)
