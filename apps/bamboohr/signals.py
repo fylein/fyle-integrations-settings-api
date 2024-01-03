35a5887216106bc2e3a8536640f3cbda419c6d3e
Ashutosh619-sudo <ashutoshsinghrkt@gmail.com>
2024-01-03 13:44:42 +0530

    fixed and tested

--- apps/bamboohr/signals.py
+++ apps/bamboohr/signals.py
@@ -18,23 +18,29 @@ def run_pre_save_configuration_triggers(sender, instance: BambooHrConfiguration,
 
 @receiver(post_save, sender=BambooHr)
 def run_post_save_bamboohr_triggers(sender, instance: BambooHr, **kwargs):
+
+    # Disconnect the signal to avoid triggering it again
+    post_save.disconnect(run_post_save_bamboohr_triggers, sender=BambooHr)
+
     bamboohrsdk = BambooHrSDK(api_token=instance.api_token, sub_domain=instance.sub_domain)
     
     webhook_payload = {
-        "postFields": {
-            "firstName": "firstName",
-            "lastName": "lastName",
-            "department": "department",
-            "workEmail": "workEmail",
-            "supervisorEmail": "supervisorEmail",
-            "status": "status"
+        'postFields': {
+            'firstName': 'firstName',
+            'lastName': 'lastName',
+            'department': 'department',
+            'workEmail': 'workEmail',
+            'status': 'status'
         },
-        "name": instance.org.name,
-        "monitorFields": ["firstName", "lastName", "department", "workEmail", "supervisorEmail", "status"],
-        "url": API_URL+f'/orgs/{instance.org.id}/bamboohr/webhook_callback/',
-        "format": "json"
+        'name': instance.org.name,
+        'monitorFields': ['firstName', 'lastName', 'department', 'workEmail', 'reportingTo', 'status'],
+        'url': API_URL + f'/orgs/{instance.org.id}/bamboohr/webhook_callback/',
+        'format': 'json'
     }
 
     response = bamboohrsdk.webhook.post(payload=webhook_payload)
     instance.webhook_id = int(response['id'])
     instance.save()
+
+    # Reconnect the signal after saving
+    post_save.connect(run_post_save_bamboohr_triggers, sender=BambooHr)
