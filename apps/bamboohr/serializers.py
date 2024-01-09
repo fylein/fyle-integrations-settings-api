485302c64d056c0735c6f98a053f71449844c380
Ashutosh619-sudo <ashutoshsinghrkt@gmail.com>
2024-01-09 16:12:02 +0530

    removing workato related views

--- apps/bamboohr/serializers.py
+++ apps/bamboohr/serializers.py
@@ -25,42 +25,15 @@ class BambooHrConfigurationSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):        
         org = validated_data['org']
-        connector = Workato()
-        managed_user_id = Org.objects.get(id=org).managed_user_id
-        recipes = connector.recipes.get(managed_user_id)['result']
-        code = json.loads(recipes[0]['code'])
-
-        admin_emails = [
-            {
-             'email': admin['email'],
-            } for admin in validated_data['emails_selected']
-        ]
-
-        code['block'][0]['block'][2]['block'][0]['input']['personalizations'][0]['to'] = admin_emails
-        code['block'][0]['block'][2]['block'][0]['input']['from']['email'] = settings.SENDGRID_EMAIL
-
-        recipes[0]['code'] = json.dumps(code)
-
-        payload = {
-            "recipe": {
-                "name": recipes[0]['name'],
-                "code": recipes[0]['code'],
-                "folder_id": str(recipes[0]['folder_id'])
-            }
-        }
+        
         configuration, _ = BambooHrConfiguration.objects.update_or_create(
             org_id=org,
-            recipe_id=recipes[0]['id'],
             defaults={
                 'recipe_status': True,
-                'recipe_data': recipes[0]['code'],
                 'additional_email_options': validated_data['additional_email_options'],
                 'emails_selected': validated_data['emails_selected']
             }
         )
-
-        connector.recipes.post(managed_user_id, configuration.recipe_id, payload)
-        connector.recipes.post(managed_user_id, configuration.recipe_id, None, 'start')
 
         return configuration
 
--- apps/bamboohr/views.py
+++ apps/bamboohr/views.py
@@ -81,65 +81,6 @@ class WebhookCallbackAPIView(generics.CreateAPIView):
             status=status.HTTP_201_CREATED
         )
 
-class PostFolder(generics.CreateAPIView):
-    """
-    API Call to Create Folder in Workato
-    """
-    serializer_class = BambooHrSerializer
-
-    def post(self, request, *args, **kwargs):
-        org = Org.objects.filter(id=kwargs['org_id']).first()
-
-        folder = post_folder(
-            org_id=kwargs['org_id'],
-            folder_name='Bamboo HR'
-        )
-
-        # in case of an error response
-        if isinstance(folder, Response):
-            return folder
-        
-        bamboohr, _ = BambooHr.objects.update_or_create(
-            org=org,
-            defaults={
-                'folder_id': folder['id']
-            }
-        )
-
-        return Response(
-            data=BambooHrSerializer(bamboohr).data,
-            status=status.HTTP_200_OK
-        )
-
-class PostPackage(generics.CreateAPIView):
-    """
-    API Call to Post Package in Workato
-    """
-
-    def post(self, request, *args, **kwargs):
-        org = Org.objects.filter(id=kwargs['org_id']).first()
-        bamboohr = BambooHr.objects.filter(org__id=org.id).first()
-
-        package = post_package(
-            org_id=kwargs['org_id'],
-            folder_id=bamboohr.folder_id,
-            package_path='assets/bamboohr_package.zip'
-        )
-
-        # in case of an error response
-        if isinstance(package, Response):
-            return package
-        
-        bamboohr.package_id = package['id']
-        bamboohr.save()
-
-        return Response(
-            data={
-                'message': 'package uploaded successfully',
-            },
-            status=status.HTTP_200_OK
-        )
-
 class BambooHrConnection(generics.CreateAPIView):
     """
     API Call to make Bamboo HR Connection in Workato
@@ -147,37 +88,19 @@ class BambooHrConnection(generics.CreateAPIView):
 
     def post(self, request, *args, **kwargs):
         org = Org.objects.filter(id=kwargs['org_id']).first()
-        bamboohr = BambooHr.objects.filter(org__id=kwargs['org_id']).first()
 
-        # creating bamboo connection for cron job that will look for new employee in bamboohr
-        bamboo_connection = create_connection_in_workato(org.id, BAMBOO_HR['connections'][0], org.managed_user_id, request.data)
-        
-        # if the connection if successfull we will go on to create the second bamboohr connection
-        # that is used for the complete sync recipe in bamboohr
-        if 'authorization_status' in bamboo_connection and bamboo_connection['authorization_status'] == 'success':
-            connection_payload = {
-                "input": {
-                    "ssl_params": "false",
-                    "auth_type": "basic",
-                    "basic_user": request.data['input']['api_token'],
-                    "basic_password": "x"
-                }
-            }
-            bamboo_sync_connection = create_connection_in_workato(org.id, BAMBOO_HR['connections'][1], org.managed_user_id, connection_payload)
-            bamboohr.api_token = request.data['input']['api_token']
-            bamboohr.sub_domain = request.data['input']['subdomain']
-            bamboohr.save()
+        api_token = request.data['input']['api_token']
+        sub_domain = request.data['input']['subdomain']
 
-            return Response(
-                data=bamboo_sync_connection,
-                status=status.HTTP_200_OK
-            )
-        elif 'authorization_status' in bamboo_connection:
-            return Response(
-                bamboo_connection,
-                status = status.HTTP_400_BAD_REQUEST
-            )
-        return bamboo_connection
+        bamboohr = BambooHr.objects.get_or_create(org=org, defaults={
+            'api_token': api_token,
+            'sub_domain': sub_domain
+        }) 
+
+        return Response(
+            data="BambooHr is connected",
+            status=status.HTTP_200_OK
+        )
 
 
 class BambooHrConfigurationView(generics.ListCreateAPIView):
