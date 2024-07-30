2d6d5e31d4b36fbdc9740ee3fe90cee8e2480fa9
Hrishabh Tiwari <hrishabh.t@fyle.in>
2024-07-30 17:32:52 +0530

    add loggers to connection payload

--- apps/bamboohr/views.py
+++ apps/bamboohr/views.py
@@ -91,8 +91,11 @@ class BambooHrConnection(generics.CreateAPIView):
         api_token = request.data['input']['api_token']
         sub_domain = request.data['input']['subdomain']
 
+        logger.info('Bamboo HR Connection Request Payload | Content: {{api_token: {0}, sub_domain: {1}}}'.format(api_token, sub_domain))
+
         bamboohrsdk = BambooHrSDK(api_token=api_token, sub_domain=sub_domain)
         timeoff = bamboohrsdk.time_off.get()
+        logger.info('Bamboo HR Connection Timeoff Payload | Content: {0}'.format(timeoff))
         if timeoff.get('timeOffTypes', None):
             bamboohr, _ = BambooHr.objects.update_or_create(org=org, defaults={
                 'api_token': api_token,
