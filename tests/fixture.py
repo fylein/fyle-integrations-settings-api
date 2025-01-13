d88df20c34eec5e72fb040e4266fa239681efbf8
jayachandhar <jayachandhar.jr@fyle.in>
2025-01-13 20:00:36 +0530

    fix: redact S3 URL in test data

--- tests/fixture.py
+++ tests/fixture.py
@@ -53,7 +53,7 @@ fixture = {
         ],
         'reference':'Trip #10205',
         'travelperk_bank_account':None,
-        'pdf':'https://tk-sandbox-backend-data.s3.amazonaws.com/invoices/2067/INV-04-00439.pdf?AWSAccessKeyId=ASIA5MFYTLYHZAORGIWU&Signature=uELtA4ryfUv%2Funzoc%2FHZ2UnoCM4%3D&x-amz-security-token=IQoJb3JpZ2luX2VjEBMaCWV1LXdlc3QtMSJHMEUCIQDOJ80ygUASFyGjkKYPUThjr0IW%2FOGrcj33cBfzwbPBWAIgUQaag3KYqW18d%2FFZMkC3adMAb4eeK3fRENKkL%2Ffu82Mq5QMIHBABGgw5MTk1MTAxNDY1NzUiDMUxxgxfOTNFwERY3SrCA%2Fd7OSKEzxz9vzfahtvLYEMfVUAzjLK7umU4KBsT2Bu5J%2FD7NJfleDIxTanblvdNOxMgppDf9lWyFYS6ZXv5iG6GCyi1jWRhFBPQfjHZAzMVe6Ctyriz8l8hfxv5u30QJdqHJ5ReoGi6b6KAT7zbSS84cN5OjnYF4xt%2BWY%2BToXw2PQ%2FFEXTbK%2BSjV5KYtAMQ0yu%2FKec0ifqyounRnbEOaxHHVoRprryMidCYtlKABNZRBIsbJ0kcXrZyY3XLZHLHyvm7OCIbLfuVQhnjKYbcrTfLg7sTqoNlYQJ56J%2F%2F1PZjC2TwNXEQg4Yhzz1EBKSK5Uze5xTiNYjuJDERxbHVvFVze9yd0TWkXl5W5kGfwyWKxsM35IOZQpxgNvQEKCeFck4e9hLKDvYOj8i0HA5bgIMb5SAeyuPXD58jKTrupRbyvZ4pS0%2FREJnecxZmaa12V6UL4UnSf8VgcTebTzVogInyy7o1pZh%2FFBKqsk4eWQSzR6ATa3QeLOoAaF%2FHC%2FyUbPz3o5UmJOzeuIZxwtPuBmps%2FVQXpSiE%2F8Nm2xRHcbVPNqdSfdQtqse58TGqYt4weIDoHgopZtuZ1Sfmdg2wP49mYzDkh7ivBjqlAQQlPWchxCJvKIY5rsE2qkAQ45TI0xNIdXdpPm%2Fq9EIo1KjRonXZ1kl0TXF2vsvjMGunKo%2Bdzc6yvF8zhhO0%2BI5OHF%2FQ4xewabEyC4BFAWzKna3IC2cpae5JVVeAON9IrXdQITClQS%2BjkAF1YUQBBpJ01cQg2%2Bg3s4m8bPK9Gl16Ekbvw8J7XAXWwhyp0VWSsn8iL5GlWoSZa7mO8QiGIqJMS68wMQ%3D%3D&Expires=1710107789',
+        'pdf':'xyz.s3.amazonaws.com/invoices/2067/INV-04-00439.pdf?AWSAccessKeyId=XXXX&Signature=uELtA4ryfUv%2Funzoc%2FHZ2UnoCM4%3D&Expires=1710107789',
         'lines':[
             {
                 'id':'4cd6abae-762b-4706-8503-28b937f50ada',
