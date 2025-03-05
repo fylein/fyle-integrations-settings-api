da8af706b501124f2a13f4c2516de3ef16cc467c
Ashutosh619-sudo <ashutosh.s@fyle.com>
2025-03-05 23:02:09 +0530

    used empty RabbitMQData

--- apps/integrations/tasks.py
+++ apps/integrations/tasks.py
@@ -1,6 +1,7 @@
 import logging
 from fyle_accounting_library.rabbitmq.connector import RabbitMQConnection
 from fyle_accounting_library.fyle_platform.enums import RoutingKeyEnum
+from fyle_accounting_library.rabbitmq.data_class import RabbitMQData
 import json
 
 logger = logging.getLogger(__name__)
@@ -12,7 +13,8 @@ def publish_to_rabbitmq():
     """
     try:
         rabbitmq = RabbitMQConnection.get_instance('integrations_pgevents_exchange')
-        rabbitmq.publish('upload_s3', None)
+        message = RabbitMQData()
+        rabbitmq.publish(RoutingKeyEnum.UPLOAD_S3, message)
         logger.info('Successfully published message to RabbitMQ')
 
     except Exception as e:
