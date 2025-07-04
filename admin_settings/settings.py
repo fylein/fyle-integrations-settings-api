b732f3a25481973bf211cb228f769575d519f92e
Ashutosh singh <55102089+Ashutosh619-sudo@users.noreply.github.com>
2025-07-04 13:35:50 +0530

    Feat: Fix throttling vulnerability (#291)
    Co-authored-by: Ashutosh619-sudo <ashutosh.s@fyle.com>

--- admin_settings/settings.py
+++ admin_settings/settings.py
@@ -110,7 +110,15 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 100
+    'PAGE_SIZE': 100,
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '10/second',
+        'user': '10/second'
+    }
 }
 
 Q_CLUSTER = {
