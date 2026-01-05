244a1e3f1f9d4565cc4ed2172b15ce7c28d8cb0e
Vignesh S - Macbook <vignesh.shankar@sage.com>
2026-01-05 14:20:34 +0530

    another

--- apps/travelperk/actions.py
+++ apps/travelperk/actions.py
@@ -184,7 +184,7 @@ def construct_expense_payload(org_id: str, expense: dict, amount: int, file_ids:
             'source': 'CORPORATE_CARD',
             'spent_at': str(datetime.strptime(expense.expense_date, "%Y-%m-%d").replace(tzinfo=timezone.utc)),
             'purpose': purpose,
-            'merchant': 'Travelperk',
+            'merchant': 'Perk',
             'category_id': category_id,
             'file_ids': file_ids
         }
